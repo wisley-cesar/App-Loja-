24cc52617c462a0451f90d996a11b46d39793084
Wisley César <wisleycesar32@gmail.com>
2024-07-23 16:52:42 -0300

    Widgets

---
+++ lib/widgets/product_grid_item.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:loja/models/cart.dart';
+import 'package:loja/models/product.dart';
+import 'package:loja/utils/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class ProductGridItem extends StatelessWidget {
+  const ProductGridItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            icon: Consumer<Product>(
+              builder: (context, product, child) => Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.name,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.tertiary,
+            onPressed: () {
+              cart.addItem(product);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Produto adicionado com sucesso!',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'DESFAZER',
+                    textColor: Theme.of(context).colorScheme.error,
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  behavior: SnackBarBehavior.floating,
+                  elevation: 6,
+                ),
+              );
+            },
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.PRODUCT_DETAIL,
+              arguments: product,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
