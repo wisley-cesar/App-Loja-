9a4043d42022875f9adb179bbb44e6479f1f149b
Wisley César <wisleycesar32@gmail.com>
2024-07-26 18:15:22 -0300

    Modelos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:loja/data/dammy_data.dart';
 import 'package:loja/models/product.dart';
@@ -13,11 +15,47 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
+  void sabeProduct(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+    final product = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'].toString(),
+    );
+
+    if (hasId) {
+      upDateProduct(product);
+    } else {
+      addProduct(product);
+    }
+  }
+
   void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
   }
+
+  void upDateProduct(Product product) {
+    int index = _items.indexWhere((prod) => prod.id == product.id);
+
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void removeProduct(Product product) {
+    int index = _items.indexWhere((prod) => prod.id == product.id);
+
+    if (index >= 0) {
+      _items.removeWhere((prod) => prod.id == product.id);
+      notifyListeners();
+    }
+  }
 }
+
 
 // bool _showFavoriteOnly = false;
 
