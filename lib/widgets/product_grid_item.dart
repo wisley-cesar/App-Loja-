cb0b6e0a8b76073df0e5bc164b4f52563b93449a
Wisley César <wisleycesar32@gmail.com>
2025-01-16 17:58:55 -0300

    FEAT: Criando uma animação de quando não tem produto.

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -77,10 +77,16 @@ class ProductGridItem extends StatelessWidget {
               arguments: product,
             );
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder:
+                const AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
+          // child: Image.network(
+          //   product.imageUrl,
+          //   fit: BoxFit.cover,
+          // ),
         ),
       ),
     );
